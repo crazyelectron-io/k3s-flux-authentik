d908fb615b93db3c191feade7ed3956153ae36ac
Ron Moerman <ron@crazyelectron.io>
2025-01-30 20:24:39 +0100

    Add variables
    Signed-off-by: Ron Moerman <ron@crazyelectron.io>

--- authentik/post-deploy-job/jobs/createusers.sh
+++ authentik/post-deploy-job/jobs/createusers.sh
@@ -3,3 +3,9 @@
 echo ".......... Installing Required Packages ............"
 apt-get update
 apt-get install --no-install-recommends --no-install-suggests curl jq ca-certificates -y
+
+# Global variables
+AUTHENTIK_URL="https://auth.moerman.online/api/v3"
+# API_TOKEN is passed in environment variable
+
+# --- Common functions
