cbd1f91ac1fe2c28ee270a7cbd491cb634b53b88
Ron Moerman <ron@crazyelectron.io>
2025-01-30 19:57:37 +0100

    Remove all lines
    Signed-off-by: Ron Moerman <ron@crazyelectron.io>

--- authentik/post-deploy-job/jobs/create-users.sh
+++ authentik/post-deploy-job/jobs/create-users.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 echo ".......... Installing Required Packages ............"
-apt-get update
-apt-get install --no-install-recommends --no-install-suggests curl jq ca-certificates -y
+# apt-get update
+# apt-get install --no-install-recommends --no-install-suggests curl jq ca-certificates -y
 
-# Global variables
-AUTHENTIK_URL="https://auth.moerman.online/api/v3"
-# API_TOKEN is passed in environment variable
+# # Global variables
+# AUTHENTIK_URL="https://auth.moerman.online/api/v3"
+# # API_TOKEN is passed in environment variable
 
 # --- Common functions
 
