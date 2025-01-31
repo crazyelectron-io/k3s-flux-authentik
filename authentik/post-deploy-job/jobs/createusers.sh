fe28b8e78d183a8b27afaef9142225fae458216b
Ron Moerman <ron@crazyelectron.io>
2025-01-31 08:33:46 +0100

    Change id parsing
    Signed-off-by: Ron Moerman <ron@crazyelectron.io>

--- authentik/post-deploy-job/jobs/createusers.sh
+++ authentik/post-deploy-job/jobs/createusers.sh
@@ -26,12 +26,21 @@ echo ".......... Creating Groups ............"
 
 response=$(make_request POST groups '{"name": "Familie", "parent": null}')
 echo "Response: $response"
-familie_group_id=$(echo $response | jq -r '.id')
+familie_group_id=$(echo $response | jq -r '.pk')
 response=$(make_request POST groups '{"name": "GrafanaAdmin", "parent": null}')
 echo "Response: $response"
-grafana_group_id=$(echo $response | jq -r '.id')
+grafana_group_id=$(echo $response | jq -r '.pk')
 echo "Familie group ID: $familie_group_id"
 echo "Grafana group ID: $grafana_group_id"
 
 echo ".......... Creating Users ............"
 
+response=$(make_request POST users '{"username": "ronmoerman","name": "Ron","is_active": true,"last_login": "","groups": ["$familie_group_id","$grafana_group_id","email": "ron@moerman.online","attributes": {},"path": "users","type": "internal"}'
+echo "Response: $response"
+user1_id = $(echo $response | jq -r '.pk')
+response=$(make_request POST users '{"username": "arnielmoerman","name": "Arniël","is_active": true,"last_login": "","groups": ["$familie_group_id"],"email": "arniel@moerman.online","attributes": {},"path": "users","type": "internal"}'
+echo "Response: $response"
+user2_id = $(echo $response | jq -r '.pk')
+response=$(make_request POST users '{"username": "mennomoerman","name": "Menno","is_active": true,"last_login": "","groups": ["$familie_group_id"],"email": "mennonm20@gmail.com","attributes": {},"path": "users","type": "internal"}'
+echo "Response: $response"
+user3_id = $(echo $response | jq -r '.pk')
