06fcb7c0285e9825947b0dad3af513a4f74efe08
Ron Moerman <ron@crazyelectron.io>
2025-01-31 07:07:44 +0100

    Add API_TOKEN var to script
    Signed-off-by: Ron Moerman <ron@crazyelectron.io>

--- authentik/post-deploy-job/jobs/createusers.sh
+++ authentik/post-deploy-job/jobs/createusers.sh
@@ -4,9 +4,10 @@ echo ".......... Installing Required Packages ............"
 apt-get update
 apt-get install --no-install-recommends --no-install-suggests curl jq ca-certificates -y
 
-# Global variables
+# --- Global variables
 AUTHENTIK_URL="https://auth.moerman.online/api/v3"
 # API_TOKEN is passed in environment variable
+echo "API token: $API_TOKEN"
 
 # --- Common functions
 
