414ad1eb2e88347139ca17726d4ada1d8518a675
Ron Moerman <ron@crazyelectron.io>
2025-01-31 09:46:27 +0100

    Change set_password URL
    Signed-off-by: Ron Moerman <ron@crazyelectron.io>

--- authentik/post-deploy-job/jobs/createusers.sh
+++ authentik/post-deploy-job/jobs/createusers.sh
@@ -38,12 +38,12 @@ echo ".......... Creating Users ............"
 response=$(make_request POST users "{\"username\": \"ronmoerman\",\"name\": \"Ron\",\"is_active\": true,\"last_login\": null,\"groups\": [\"$familie_group_id\",\"$grafana_group_id\"],\"email\": \"ron@moerman.online\",\"attributes\": {},\"path\": \"users\",\"type\": \"internal\"}")
 echo "Response: $response"
 user1_id=$(echo $response | jq -r '.pk')
-response=$(make_request POST users/$user1_id/set-password "{\"password\": \"${USER1_PASSWORD}\"}")
+response=$(make_request POST users/:$user1_id/set-password "{\"password\": \"${USER1_PASSWORD}\"}")
 echo $reponse
 response=$(make_request POST users "{\"username\": \"arnielmoerman\",\"name\": \"Arniël\",\"is_active\": true,\"last_login\": null,\"groups\": [\"$familie_group_id\"],\"email\": \"arniel@moerman.online\",\"attributes\": {},\"path\": \"users\",\"type\": \"internal\"}")
 echo "Response: $response"
 user1_id=$(echo $response | jq -r '.pk')
-response=$(make_request POST users/$user2_id/set-password "{\"password\": \"${USER2_PASSWORD}\"}")
+response=$(make_request POST users/:$user2_id/set-password "{\"password\": \"${USER2_PASSWORD}\"}")
 echo $reponse
 
 response=$(make_request POST users "{\"username\": \"mennomoerman\",\"name\": \"Menno\",\"is_active\": true,\"last_login\": null,\"groups\": [\"$familie_group_id\"],\"email\": \"mennonm20@gmail.com\",\"attributes\": {},\"path\": \"users\",\"type\": \"internal\"}")
